c109edeb7c5e194992e4fa78d6c70c274f42ef5a
Desi Vasileva <72608848+dessiVVA@users.noreply.github.com>
2022-03-07 16:07:31 +0200

    Improve css

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Card = () => {
   return (
     <div className="card">
-      <img src="" alt="" />
+      <div className="img">
+        <img
+          src="https://dl.airtable.com/.attachments/a0cd0702c443f31526267f38ea5314a1/2447eb7a/paris.jpg"
+          alt=""
+        />
+      </div>
+
       <div className="textArea">
         <h3 className="tourName">Best of Paris in 7 days</h3>
         <p className="price">$1,995</p>
